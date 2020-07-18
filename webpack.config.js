f6f95a8f36c7dfc0a916aabee88fcb11491f1d7e
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-07-18 16:16:11 +0200

    made basic setup for app

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const TSLintPlugin = require('tslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './public',
+    watchContentBase: true,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'ts-loader'],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new TSLintPlugin({
+      files: ['./src/**/*.ts'],
+    }),
+  ],
+};
