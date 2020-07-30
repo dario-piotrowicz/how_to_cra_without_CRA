17ff15567d9ae07bc399c9937d0b659a41598938
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-07-30 20:13:21 +0200

    set min-width for the whole app

--- src/app.tsx
+++ src/app.tsx
@@ -22,7 +22,7 @@ const App: FunctionComponent = () => {
   const prettier = useSelector(selectPrettier);
 
   return (
-    <>
+    <div id="app">
       <Header />
       <Introduction />
       <Settings />
@@ -34,7 +34,7 @@ const App: FunctionComponent = () => {
       {prettier ? <PrettierSetupSection /> : null}
       <Conclusions />
       <Footer />
-    </>
+    </div>
   );
 };
 
