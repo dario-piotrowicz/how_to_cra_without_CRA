64ad43fb45aea0416b93bb79ea308f255b056104
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-09 15:26:48 +0200

    added animations for files-structure-modal

--- src/components/files-structure-modal/files-structure-modal.component.tsx
+++ src/components/files-structure-modal/files-structure-modal.component.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import './files-structure-modal.styles.scss';
 import { useDispatch } from 'react-redux';
 import { hideFilesStructure } from '../../redux/files-structure/files-structure.actions';
@@ -6,16 +6,22 @@ import Icon from '@iconify/react';
 import timesIcon from '@iconify/icons-fa-solid/times';
 
 const FilesStructureModal: FunctionComponent = () => {
+  const [closing, setClosing] = useState(false);
   const dispatch = useDispatch();
 
-  const closeModal = () => dispatch(hideFilesStructure());
+  if (closing) {
+    setTimeout(() => dispatch(hideFilesStructure()), 500);
+  }
 
   return (
     <div id="files-structure-modal">
-      <div className="backdrop" onClick={closeModal} />
-      <div className="body">
+      <div
+        className={`backdrop${closing ? ' closing' : ''}`}
+        onClick={() => setClosing(true)}
+      />
+      <div className={`body${closing ? ' closing' : ''}`}>
         <header>
-          <span className="icon" onClick={closeModal}>
+          <span className="icon" onClick={() => setClosing(true)}>
             <Icon icon={timesIcon} />
           </span>
         </header>
