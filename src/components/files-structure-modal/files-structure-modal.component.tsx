d5a136e535cd6e12ff392d64b2aba028a8742571
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-10 20:07:25 +0200

    added title to files-structure-modal

--- src/components/files-structure-modal/files-structure-modal.component.tsx
+++ src/components/files-structure-modal/files-structure-modal.component.tsx
@@ -21,6 +21,10 @@ const FilesStructureModal: FunctionComponent = () => {
       />
       <div className={`body${closing ? ' closing' : ''}`}>
         <header>
+          <h2 className="title">
+            If you've been following the code at this point your project's files
+            structure should be as follows:
+          </h2>
           <span className="icon" onClick={() => setClosing(true)}>
             <Icon icon={timesIcon} />
           </span>
