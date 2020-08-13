770d21f1c5c55a61daaadd2b0f105ba5f0291bfd
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-13 20:25:17 +0200

    making app non-scrollable when files-structure modal is open

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export const persistor = persistStore(store);
